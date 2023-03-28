073c96835782603c0673b6cf10ebcb1fe2c72d48
MelTrevelyan <dtolstonogova@yandex.ru>
2023-03-28 16:02:57 +0700

    добавление логики в UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -2,11 +2,14 @@ package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -22,33 +25,43 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public Map<Long, User> getUsers() {
-        return null;
+        Map<Long, User> users = new HashMap<>();
+        String sqlQuery = "SELECT * FROM \"USER\"";
+        List<User> usersFromDb = jdbcTemplate.query(sqlQuery, this::mapRowToUser);
+        for (User user : usersFromDb) {
+            users.put(user.getId(), user);
+        }
+        return users;
     }
 
     @Override
     public User create(User user) {
-        return null;
+        String sqlQuery = "INSERT INTO \"USER\" (EMAIL, LOGIN, BIRTHDAY, NAME) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sqlQuery, user.getEmail(), user.getLogin(), user.getBirthday(), user.getName());
+        return user;
     }
 
     @Override
     public User update(User user) {
-        return null;
+        String SqlQuery = "UPDATE \"USER\" SET EMAIL = ?, LOGIN = ?, BIRTHDAY = ?, NAME = ? WHERE USER_ID = ?";
+        jdbcTemplate.update(SqlQuery, user.getEmail(), user.getLogin(), user.getBirthday(), user.getName());
+        return user;
     }
 
     @Override
     public User findUserById(long id) {
-        SqlRowSet userRows = jdbcTemplate.queryForRowSet("SELECT * FROM USER WHERE USER_ID = ?", id);
-        if (userRows.next()) {
-            log.info("Найден пользователь c id = {}", userRows.getLong("USER_ID"));
-            return User.builder()
-                    .email(userRows.getString("EMAIL"))
-                    .login(userRows.getString("LOGIN"))
-                    .name(userRows.getString("NAME"))
-                    .id(userRows.getLong("USER_ID"))
-                    .birthday(Objects.requireNonNull(userRows.getDate("BIRTHDAY")).toLocalDate())
-                    .build();
-        }
-        return null;
+        String sqlQuery = "SELECT * FROM \"USER\" WHERE USER_ID = ?";
+        return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToUser, id);
+    }
+
+    private User mapRowToUser(ResultSet rs, int rowNum) throws SQLException {
+        return User.builder()
+                .email(rs.getString("EMAIL"))
+                .login(rs.getString("LOGIN"))
+                .name(rs.getString("NAME"))
+                .id(rs.getLong("USER_ID"))
+                .birthday(Objects.requireNonNull(rs.getDate("BIRTHDAY")).toLocalDate())
+                .build();
     }
 }
 
